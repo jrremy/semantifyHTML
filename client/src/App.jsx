1f7bbc50eed4be59722e9e8d0947107d1a56e08e
Jeremy Davoodzadeh <jeremy@effyjewelry.com>
2024-07-25 17:06:44 -0400

    UI changes, added menu buttons for modes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 import "./App.css";
 
 function App() {
+  const [selectedModeButtonIndex, setSelectedModeButtonIndex] = useState(0);
   const [outputHTML, setOutputHTML] = useState("");
   const [inputHTML, setInputHTML] = useState("");
 
@@ -14,25 +15,46 @@ function App() {
     setOutputHTML((await response).data.semantic);
   };
 
-  function handleClick() {
+  function handleConvertButtonClick() {
     fetchSemanticHTML();
+  }
+
+  function handleModeButtonClick(index) {
+    setSelectedModeButtonIndex(index);
   }
 
   function handleInputChange(event) {
     setInputHTML(event.target.value);
   }
 
-  /*useEffect(() => {
-    
-  }, []);*/
+  const inputModes = ["Paste HTML", "Paste URL"];
 
   return (
     <>
+      <div className="menu-container">
+        <menu>
+          {inputModes.map((button, index) => (
+            <button
+              key={index}
+              className={`menu-button ${
+                selectedModeButtonIndex === index ? "selected" : ""
+              }`}
+              onClick={() => handleModeButtonClick(index)}
+            >
+              {button}
+            </button>
+          ))}
+        </menu>
+      </div>
       <div class="input-area">
         <h3>Input HTML code here:</h3>
-        <textarea class="input-output-boxes" value={inputHTML} onChange={handleInputChange}></textarea>
+        <textarea
+          class="input-output-boxes"
+          value={inputHTML}
+          onChange={handleInputChange}
+        ></textarea>
       </div>
-      <button onClick={handleClick}>Convert</button>
+      <button onClick={handleConvertButtonClick}>Convert</button>
       <div class="output-area">
         <h3>Converted Semantic HTML:</h3>
         <textarea class="input-output-boxes" value={outputHTML}></textarea>
